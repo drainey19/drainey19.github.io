e09a0153191bfc4e3311e70c32d9403ebffb6539
d_rainey19 <drainey19@georgefox.edu>
2021-09-28 15:05:56 -0700

    personal project: light/dark mode

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function changeColor(color) {
     document.body.style.background = color;
 }
 
+function changeTextColor(color2) {
+    document.body.style.color = color2;
+}
+
+// function to turn background to white
 function lightMode() {
-    changeColor('white');  
+    changeColor('white');
+    changeTextColor('black')
+}
+
+// creates a function to turn background to dark grey
+function darkMode() {
+    changeColor('#121212');
+    changeTextColor('white');
 }
