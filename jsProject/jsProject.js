0464a144bf8063e0532974760d937ee12f49d117
d_rainey19 <drainey19@georgefox.edu>
2021-11-07 20:26:41 -0800

    JS Project: Made it functional

--- jsProject/jsProject.js
+++ jsProject/jsProject.js
@@ -21,4 +21,28 @@ function darkMode() {
     changeTextColor('white');
 }
 
-// music facts
+// song lyrics
+
+function findLyrics () {
+    $.get("https://api.lyrics.ovh/v1/" + document.getElementById("artist").value + "/" + 
+    document.getElementById("title").value,
+    function(data) {
+        console.log(data);
+        document.getElementById("output").innerHTML=data.lyrics.replace(new RegExp("\n", "g"), "<br>");
+    })
+}
+
+/*
+function findLyrics() {
+    let requestData = document.getElementById("artist").value + "/" + document.getElementById("title").value;
+    $.ajax({
+        url: "https://api.lyrics.ovh/v1/",
+        method: "GET",
+        data: requestData,
+    })
+    .done(function(data) {
+        
+        document.getElementById("output").innerHTML=data.lyrics.replace(new RegExp("\n", "g"), "<br>");
+    });
+}
+*/
