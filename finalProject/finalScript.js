e4f4b579abe0b544169646a8de0cafa47e65ba40
d_rainey19 <drainey19@georgefox.edu>
2021-11-21 21:55:03 -0800

    Final Project: added show hide

---
+++ finalProject/finalScript.js
@@ -0,0 +1,17 @@
+
+// add functions to show the games 
+function playOregonTrail() {
+    $("#snakeGame").hide();
+    $("#oregonTrail").show();
+}
+
+function playSnakeGame() {
+    $("#oregonTrail").hide();
+    $("#snakeGame").show();
+}
+
+// snake game
+
+
+
+// api
