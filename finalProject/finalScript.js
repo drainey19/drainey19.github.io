3d50b22031fac6544ca15f0c2e879da1363569e8
Dylan Rainey <dylanrainey@Dylans-MacBook-Air.local>
2021-11-22 12:02:25 -0800

    Laptop change

--- finalProject/finalScript.js
+++ finalProject/finalScript.js
@@ -11,14 +11,48 @@ function playSnakeGame() {
 }
 
 // snake game
+// draw bord
+const boardBackground = 'white';
+const boardBorder = 'black';
+const snakeColor = 'red';
+const snakeBorder = 'black';
 
 // create board
+const snakeBoard = document.getElementById("#snakeCanvas");
+const snakeContext = snakeBoard.getContext("2d");
 
 // create snake starting cordinates
+let snake = [
+    {x: 250, y: 250}, {x: 240, y: 250}, {x: 230, y:250}, {x: 220, y: 250}, {x:210,y:250},
+];
 
-// function to draw the snake parts
+// start game
+main();
+
+// Keeps the game going
+function main() {
+    drawCanvas();
+    drawSnake();
+}
+
+function drawCanvas() {
+    snakeContext.fillStyle = boardBackground;
+    snakeContext.styleStroke = boardBorder;
+    snakeContext.fillRect(0, 0, snakeBoard.width, snakeBoard.height);
+    snakeContext.strokeRect(0, 0, snakeBoard.width, snakeBoard.height);
+}
 
 // function to draw the snake
+function drawSnake() {
+    snake.foreach(drawSnakePart);
+}
+
+// function for each snake part
+function drawSnakePart(snakePart) {
+    snakeContext.fillStyle = snakeColor;
+    snakeContext.styleStroke = snakeBorder;
+    snakeContext.fillRect(snakePart, snakePart, 10, 10);
+}
 
 // make snake move automatically
 
